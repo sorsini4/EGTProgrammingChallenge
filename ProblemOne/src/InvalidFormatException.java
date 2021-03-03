077f92bb2831b57a69cfbd9d0ef739353eba0430
Steve <sorsini4@yahoo.com>
2021-03-03 10:44:23 -0500

    javadoc for exception classes in P1 and P3

--- ProblemOne/src/InvalidFormatException.java
+++ ProblemOne/src/InvalidFormatException.java
@@ -1,4 +1,9 @@
-
+/**
+ * This is an exception class used for when the user enters
+ * an invalid formatted exception. In this case it is for when
+ * the user enters something that is not a valid name, this exception
+ * is thrown.
+ */
 public class InvalidFormatException extends RuntimeException{
 	public InvalidFormatException(String message) {
 		super(message);
--- ProblemThree/src/InvalidFormatException.java
+++ ProblemThree/src/InvalidFormatException.java
@@ -1,4 +1,7 @@
-
+/**
+ * This is an exception used for when the user input is in an invalid form.
+ * For this case, it is when the user enters an incorrectly formatted Phone or Social number.
+ */
 public class InvalidFormatException extends RuntimeException{
 	public InvalidFormatException(String message) {
 		super(message);
